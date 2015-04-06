7c58a3343ac1ce8fcdb05e9e445d640c1cc19252
Ian Rees <ian@ianrees.net>
2015-04-06 12:07:29 -0700

    Updated url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   description='Mapzen Geohash',
   author='Ian Rees',
   author_email='ian@mapzen.com',
-  url='http://mapzen.com/',
+  url='https://github.com/transitland/mapzen-geohash',
   license='License :: OSI Approved :: MIT License',
   packages=['mzgeohash']
 )
